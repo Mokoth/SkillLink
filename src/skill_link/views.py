8c8a893f928292dd9e2846596ed57cb7cc745b83
Mark <86335247+Mokoth@users.noreply.github.com>
2024-09-17 05:57:20 +0300

    Delete src directory

--- src/skill_link/views.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-from django.shortcuts import render
-from django.http import HttpResponse
-
-this_dir = pathlib.Path(__file__).resolve().parent
-
-def home_page_view(request, *args, **kwargs):
-    html_template = "home.html"
-    return render(request, html_template)
