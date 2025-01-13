d65cb9cef60f673df91820f325ce7ec5716851af
Flockaman <42209221+flocka12@users.noreply.github.com>
2025-01-13 17:29:07 +0300

    Login,Sign Up, Dashboard
    Added Login And Sign Up Page, Created A Dammy Dashboard.
    
    Next to Interact With Javascript

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,5 @@
 const forms = document.querySelector(".forms"),
-  pwShowHide = document.querySelectorAll(".eye-icon"),
-  links = document.querySelectorAll(".link");
+  pwShowHide = document.querySelectorAll(".eye-icon");
 
 
 // Add click event listener to each eye icon for toggling password visibility
@@ -17,14 +16,6 @@ pwShowHide.forEach(eyeIcon => {
       password.type = "password"; // Hide password
       eyeIcon.classList.replace("bx-show", "bx-hide"); // Change icon to hide state
     });
-
   });
 });
 
-// Add click event listener to each link to toggle between forms
-links.forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault(); // Prevent default link behavior
-    forms.classList.toggle("show-signup");
-  });
-});
